9b8af7c15fff0da8d459bcbab2c7f301d00c0a72
pedrohsxavier <pedro.hsxavier@outlook.com>
2019-11-22 02:27:59 -0300

    add carro model

--- src/main/java/model/Carro.java
+++ src/main/java/model/Carro.java
@@ -1,4 +1,27 @@
 package model;
 
+import lombok.Data;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Data
 public class Carro {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private int ano;
+
+    @Column(nullable = false)
+    private BigDecimal valor;
+
+    @OneToOne
+    @JoinColumn(name = "montadora_id", nullable = false, foreignKey = @ForeignKey(name = "fk_montadora_carro_id"))
+    private Montadora montadora;
 }

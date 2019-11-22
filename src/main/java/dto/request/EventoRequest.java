ee22ef218663fe41fa4a1b089451bdf1f7e0b95c
pedrohsxavier <pedro.hsxavier@outlook.com>
2019-11-22 03:35:57 -0300

    add evento request

---
+++ src/main/java/dto/request/EventoRequest.java
@@ -0,0 +1,17 @@
+package dto.request;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+public class EventoRequest {
+    private String nome;
+    private String cidade;
+    private String pais;
+    private Long usuarioId;
+    private Long montadoraId;
+    private LocalDateTime dataEvento;
+    private BigDecimal valor;
+}

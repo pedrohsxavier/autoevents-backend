671bbc17dc2bed01b7e11024a2987566f413e67f
pedrohsxavier <pedro.hsxavier@outlook.com>
2019-11-22 02:56:37 -0300

    add usuario model

--- src/main/java/model/Usuario.java
+++ src/main/java/model/Usuario.java
@@ -1,4 +1,26 @@
 package model;
 
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Data
 public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private Date dataNascimento;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String senha;
 }

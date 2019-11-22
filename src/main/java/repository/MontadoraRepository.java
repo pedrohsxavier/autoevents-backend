0552ae1f554e7782eb9be593772d59dca3fb971e
pedrohsxavier <pedro.hsxavier@outlook.com>
2019-11-22 03:19:13 -0300

    add montadora repository

---
+++ src/main/java/repository/MontadoraRepository.java
@@ -0,0 +1,7 @@
+package repository;
+
+import model.Montadora;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MontadoraRepository extends JpaRepository<Montadora, Long> {
+}

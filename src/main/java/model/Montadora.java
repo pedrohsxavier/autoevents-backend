dbbd1911c57d637b163edb1da8fb7255635a641c
pedrohsxavier <pedro.hsxavier@outlook.com>
2019-11-22 02:52:24 -0300

    add montadora model

--- src/main/java/model/Montadora.java
+++ src/main/java/model/Montadora.java
@@ -1,4 +1,19 @@
 package model;
 
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
 public class Montadora {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private String pais;
 }

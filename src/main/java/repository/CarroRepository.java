eb286280f08b9a761bb4f98457643689b0f9b0a5
pedrohsxavier <pedro.hsxavier@outlook.com>
2019-11-22 03:05:16 -0300

    add carro repository

--- src/main/java/repository/CarroRepository.java
+++ src/main/java/repository/CarroRepository.java
@@ -1,4 +1,7 @@
 package repository;
 
-public class CarroRepository {
+import model.Carro;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CarroRepository extends JpaRepository<Carro, Long> {
 }

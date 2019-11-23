05ba736913037d0486f6a1bda901fd02f237902a
pedrohsxavier <pedro.hsxavier@outlook.com>
2019-11-23 17:09:38 -0300

    add usuario service

---
+++ src/main/java/service/UsuarioService.java
@@ -0,0 +1,64 @@
+package service;
+
+import dto.request.UsuarioRequest;
+import model.Usuario;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import repository.UsuarioRepository;
+
+import java.util.Optional;
+
+@Service
+public class UsuarioService {
+    private final UsuarioRepository usuarioRepository;
+
+    public UsuarioService(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    public Usuario criarUsuario(UsuarioRequest request) {
+        Usuario usuario = this.usuarioRepository.findByEmail(request.getEmail()).orElseThrow(
+                () -> new HandlerException("E-mail já cadastrado na base de dados"));
+
+        usuario.setDataNascimento(request.getDataNascimento());
+        usuario.setEmail(request.getEmail());
+        usuario.setNome(request.getNome());
+        usuario.setSenha(request.getSenha());
+
+        return usuario;
+    }
+
+    public Usuario atualizarUsuario(Long id, UsuarioRequest request) {
+        Usuario usuario = this.usuarioRepository.findById(id).orElseThrow(
+                () -> new HandlerException("Usuário inexistente"));
+
+        Optional<Usuario> busca = this.usuarioRepository.findByEmail(request.getEmail());
+
+        if(!busca.isPresent()) {
+            usuario.setEmail(request.getEmail());
+        }
+
+        usuario.setDataNascimento(request.getDataNascimento());
+        usuario.setNome(request.getNome());
+        usuario.setSenha(request.getSenha());
+
+        return usuario;
+    }
+
+    public Page<Usuario> listarUsuarios(Pageable pageable) {
+        return this.usuarioRepository.findAll(pageable);
+    }
+
+    public Usuario buscarUsuario(Long id) {
+        Usuario usuario = this.usuarioRepository.findById(id).orElseThrow(
+                () -> new HandlerException("Usuário inexistente"));
+        return usuario;
+    }
+
+    public void removerUsuario(Long id) {
+        Usuario usuario = this.usuarioRepository.findById(id).orElseThrow(
+                () -> new HandlerException("Usuário inexistente"));
+        this.usuarioRepository.delete(usuario);
+    }
+}

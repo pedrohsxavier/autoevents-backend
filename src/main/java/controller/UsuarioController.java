9b16731ec6687e314775b8a46dea7ca3691aa9ce
pedrohsxavier <pedro.hsxavier@outlook.com>
2019-11-23 20:00:58 -0300

    update usuario controller

--- src/main/java/controller/UsuarioController.java
+++ src/main/java/controller/UsuarioController.java
@@ -1,9 +1,42 @@
 package controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import dto.request.UsuarioRequest;
+import dto.response.UsuarioResponse;
+import model.Usuario;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import service.UsuarioService;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/usuarios")
 public class UsuarioController {
+    private UsuarioService service;
+
+    public UsuarioController(UsuarioService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public ResponseEntity<UsuarioResponse> cadastrarUsuario(@Valid @RequestBody UsuarioRequest request) {
+        Usuario usuario = this.service.criarUsuario(request);
+        return ResponseEntity.ok(UsuarioResponse.from(usuario));
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<UsuarioResponse>> listarUsuarios(Pageable pageable) {
+        Page<Usuario> usuarios = this.service.listarUsuarios(pageable);
+        return ResponseEntity.ok(UsuarioResponse.from(usuarios));
+    }
+
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> removerUsuario(@Valid @PathVariable(value = "id") Long id) {
+        this.service.removerUsuario(id);
+        return ResponseEntity.ok().build();
+    }
 }

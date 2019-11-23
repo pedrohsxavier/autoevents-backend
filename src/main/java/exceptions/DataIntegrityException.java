8a9f6f6389450aa84ae223d1ebba9eb058090bbb
pedrohsxavier <pedro.hsxavier@outlook.com>
2019-11-23 18:07:28 -0300

    add data integrity exception

---
+++ src/main/java/exceptions/DataIntegrityException.java
@@ -0,0 +1,13 @@
+package exceptions;
+
+public class DataIntegrityException extends RuntimeException {
+    private static final long serialVerionUID = 1L;
+
+    public DataIntegrityException(String message) {
+        super(message);
+    }
+
+    public DataIntegrityException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}

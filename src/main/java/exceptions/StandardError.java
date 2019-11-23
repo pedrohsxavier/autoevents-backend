5267fef84223d2142dde0b7fd43b94945490cd8b
pedrohsxavier <pedro.hsxavier@outlook.com>
2019-11-23 17:58:59 -0300

    add standard error exception

---
+++ src/main/java/exceptions/StandardError.java
@@ -0,0 +1,22 @@
+package exceptions;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class StandardError implements Serializable {
+    private Long timestamp;
+    private Integer status;
+    private String error;
+    private String message;
+    private String path;
+
+    public StandardError(Long timestamp, Integer status, String error, String message, String path) {
+        this.timestamp = timestamp;
+        this.status = status;
+        this.error = error;
+        this.message = message;
+        this.path = path;
+    }
+}
